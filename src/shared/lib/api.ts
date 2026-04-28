ac77d933e792b237e12272fc910bdb10f4c1a016
Luis Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2026-04-28 13:13:25 -0300

    feat: add fake api

---
+++ src/shared/lib/api.ts
@@ -0,0 +1,20 @@
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export async function api(endpoint: string, options?: RequestInit) {
+  const response = await fetch(`${BASE_URL}${endpoint}`, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      ...options?.headers,
+    },
+  });
+
+  console.log(response);
+
+  if (!response.ok) {
+    const error = await response.json().catch(() => ({}));
+    throw new Error(error.message || "Erro na requisição");
+  }
+
+  return response.json();
+}
