cbe262877328acefc3b6e67f0a4c8100321de076
Luis Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2026-04-30 15:15:16 -0300

    style: adjust warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" data-scroll-behavior="smooth">
       <body>
         <CartProvider>
           <Header />
